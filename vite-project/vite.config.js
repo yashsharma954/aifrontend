5920fe2ddd33f5299842c00901aa849453e330a7
yashsharma954 <yashsharmarohit21@gmail.com>
2026-06-29 21:49:22 +0530

    frontend ko dobara update kiya ha

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,10 +1,18 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // httpss://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),
+//     tailwindcss(),
+//   ],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// httpss://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
 })
